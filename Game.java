297f5a54781d0b02e8814ea822b49f2ed7522d86
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2022-08-09 15:06:28 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -7,24 +7,79 @@ import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import java.util.*;
 import java.io.*;
+import java.util.Scanner;
 
-public class Game extends Application
+public class Game
 {
     public static void main(String[] args)
     {
-        launch(args);
-    }
-
-    public void start(Stage stage)
-    {
-        ArrayList<Card> deck = makeDeck();
-        for (Card c: deck)
+        boolean play = true;
+        if (play == true)
         {
-            System.out.println(c);
+            startGame();
         }
     }
 
-    public ArrayList<Card> makeDeck()
+    public static void startGame()
+    {
+        ArrayList<Card> deck = makeDeck();
+        ArrayList<Player> playerList = new ArrayList<Player>();
+        playerList.add(new Player("Player1"));
+        playerList.add(new Player("Player2"));
+        for (Player p : playerList)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                p.addCard(deck.get(deck.size() - 1));
+                deck.remove(deck.size() - 1);
+                
+            }
+            System.out.println(p.getName() + "'s" + " hand: " + p.getHand());
+        }
+
+    }
+
+    public static void startGame(ArrayList<Card> deck, ArrayList<Player> playerList)
+    {
+        ArrayList<Card> pile = new ArrayList<Card>();
+        pile.add(deck.get(deck.size() - 1));
+        System.out.println(pile.size() - 1);
+        deck.remove(deck.size()-1);
+        Scanner myObj = new Scanner(System.in);
+        while (true)
+        {
+            for (int i = 0; i < playerList.size(); i++) 
+            {
+                Player player = playerList.get(i);
+                System.out.println("Please input the position of the card you want to play");
+                int cardPos = myObj.nextInt() - 1;
+                Card playedCard = player.getHand().get(cardPos);
+                if (playedCard.isPlayable(pile.get(pile.size() -1))) 
+                {
+                    pile.add(playedCard);
+                    player.removeCard(cardPos);
+                    if (playedCard.getType().equals("Reverse")) 
+                    {
+                        
+                    } else if (playedCard.getType().equals("Skip")) {
+
+                    } else if (playedCard.getType().equals("+2")) {
+                        
+                    } else if (playedCard.getType().equals("ChangeColor")) {
+                        
+                    } else {
+
+                    }
+                }
+                
+                if (player.checkWin()) {
+                    return;
+                }
+            }
+        }
+    }
+
+    public static ArrayList<Card> makeDeck()
     {
         ArrayList<Card> deck = new ArrayList<Card>();
         int[] number = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
