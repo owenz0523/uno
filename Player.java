114d3c46a9ec297733c255342d8d89561bdc1d5f
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2022-08-09 15:06:43 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,42 @@
-public class Player {
-    
+import java.util.*;
+
+public class Player 
+{
+    private ArrayList<Card> hand;
+    private String name;
+
+    public Player(String name)
+    {
+        hand = new ArrayList<Card>();
+        this.name = name;
+    }
+
+    public void addCard(Card c)
+    {
+        hand.add(c);
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public ArrayList<Card> getHand()
+    {
+        return hand;
+    }
+
+    public void removeCard(int pos)
+    {
+        hand.remove(pos);
+    }
+
+    public boolean checkWin()
+    {
+        if (hand.size()== 0) {
+            System.out.println(name + " has won!");
+            return true;
+        }
+        return false;
+    }
 }
